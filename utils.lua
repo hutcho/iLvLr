df056e8a5a5dbca6183693ff2b21deaae09c4e88
hutcho <hutcho@users.noreply.github.com>
2025-07-16 01:24:53 +1000

    removes unused splitstr function
    adds dump table function

--- utils.lua
+++ utils.lua
@@ -50,29 +50,15 @@ function utils:get_enchantid_for_slotname(slotName)
 end
 
 
-function utils:SplitString(itemString)
-    -- itemString = 222817:7407:::::::80:72::13:5:10421:9633:8902:9624:10222:6:28:2734:29:40:30:32:38:5:40:2352:46:211296::::Player-3725-09D2C3B8:
-    local list = {}
-    local basestring = itemString .. ":"
-    -- [] is a char-set. it matches all characters inside []
-    -- ^ is a complement operator. so ^: means any character that is not :
-    -- * means 0 or more repeats
-    local pattern = "([^:]*):"
-    -- return a string in which basestring has ALL occurences of pattern
-    --  function is called every time a match occurs
-    -- with all captured substrings passed as arguments
-    -- in order; if the pattern specifies no captures, then the whole match is passed as a sole argument.
-
-    local repl = function(match) table.insert(list, match) end
-    local _ = string.gsub(basestring, pattern, repl);
-
-    -- parts = {}
-    -- for part in string.gfind(itemString, "[^:]") do
-    --   table.insert(parts, part)
-    -- end
-
-    -- or try
-    -- _, _, gemID1, gemID2, gemID3, gemID4  = strsplit(":", itemLink)
-
-    return list
+function utils:dump(o)
+    if type(o) == 'table' then
+       local s = '{ '
+       for k,v in pairs(o) do
+          if type(k) ~= 'number' then k = '"'..k..'"' end
+          s = s .. '['..k..'] = ' .. utils:dump(v) .. ','
+       end
+       return s .. '} '
+    else
+       return tostring(o)
+    end
 end
